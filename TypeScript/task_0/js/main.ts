4378c6025b47905d413bc3355794675326ec4942
Stefani-web <stephaniecvlho@gmail.com>
2024-09-23 13:10:21 +0200

    Delete comments

--- TypeScript/task_0/js/main.ts
+++ TypeScript/task_0/js/main.ts
@@ -1,12 +1,9 @@
-// Define the Student interface
 interface Student {
     firstName: string;
     lastName: string;
     age: number;
     location: string;
 }
-
-// Create two student variables
 const student1: Student = {
     firstName: 'John',
     lastName: 'Doe',
@@ -33,28 +30,22 @@ function renderTable(students: Student[]): void {
     students.forEach((student) => {
         const row = document.createElement('tr');
 
-        // Create cell for firstName
         const firstNameCell = document.createElement('td');
         firstNameCell.textContent = student.firstName;
 
-        // Create cell for location
         const locationCell = document.createElement('td');
         locationCell.textContent = student.location;
 
-        // Append cells to row
         row.appendChild(firstNameCell);
         row.appendChild(locationCell);
 
-        // Append row to the table body
         tableBody.appendChild(row);
     });
 
-    // Append the table body to the table
     table.appendChild(tableBody);
 
-    // Append the table to the document body or a specific container
+
     document.body.appendChild(table);
 }
 
-// Render the table with the studentsList
 renderTable(studentsList);
